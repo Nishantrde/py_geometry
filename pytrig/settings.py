be85cbbd8975a39afd8d9dcb5fc1102ccc8c2ac2
Nishant <nishant2024rde@gmail.com>
2024-03-02 18:25:41 -0800

    Initial Commit

--- pytrig/settings.py
+++ pytrig/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&6h5o(+f=2e!@qx(yk(xug_6(3q@per&%cx78e2f^lcb2eoszp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 # Application definition
 
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static') 
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- pytrig/urls.py
+++ pytrig/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+from django.conf import settings 
+from django.contrib import admin
+from django.conf.urls.static import static
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pyTheta.urls'))
 ]
+urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
